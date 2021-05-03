fa85996933fd33eb8c0af73700fbfe840ede6383
Yejin Kim <elena.kim@devncore.org>
2021-05-03 23:00:25 +0900

    Update Sample

---
+++ IValueConverterSample/IValueConverterSample/Converters/MultiValueBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace IValueConverterSample.Converters
+{
+    public class MultiValueBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)values[0] == true && !string.IsNullOrWhiteSpace(values[1]?.ToString()));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
