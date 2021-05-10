a2cfa4f63fda5a79a5f9426ad99861414cf33240
Yejin Kim <elena.kim@devncore.org>
2021-05-10 22:02:21 +0900

    Update Source

--- IValueConverterSample/IValueConverterSample/Converters/FileSizeToFormatConverter.cs
+++ IValueConverterSample/IValueConverterSample/Converters/FileSizeToFormatConverter.cs
@@ -4,33 +4,32 @@ using System.Windows.Data;
 
 namespace IValueConverterSample.Converters
 {
-    public class FileSizeToFormatConverter : IValueConverter
+public class FileSizeToFormatConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        long len = 0;
+        if (value.ToString() != "")
+		{
+            len = long.Parse(value.ToString());
+		}
+
+        int order = 0;
+        while (len >= 1024 && order < sizes.Length - 1)
         {
-            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
-            //long len = (long)value;
-            long len = 0;
-            if (value.ToString() != "")
-			{
-                len = long.Parse(value.ToString());
-			}
-
-            int order = 0;
-            while (len >= 1024 && order < sizes.Length - 1)
-            {
-                order++;
-                len = len / 1024;
-            }
-
-            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
-            // show a single decimal place, and no space.
-            return String.Format("({0:0.##} {1})", len, sizes[order]);
+            order++;
+            len = len / 1024;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+        // show a single decimal place, and no space.
+        return String.Format("({0:0.##} {1})", len, sizes[order]);
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
 }
